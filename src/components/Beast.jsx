8f329e5d5bf933aef33ff1d48d9b117a4c021333
Brian Kimbrow <bkimbrow25@gmail.com>
2023-07-13 00:19:24 -0500

    update

--- src/components/Beast.jsx
+++ src/components/Beast.jsx
@@ -1,28 +1,32 @@
 import React from "react";
 
 class Beast extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        favoriteCount:0,
-      };
-    }
-
-    handlefavorite = () => {
-      this.setState ({
-        favoriteCount: this.state.favoriteCount + 1,
-      });
+  constructor(props) {
+    super(props);
+    this.state = {
+      favoriteCount: 0,
     };
+  }
 
-    render() {
-      return (
-       <div>
+  handleFavorite = () => {
+    this.setState({
+      favoriteCount: this.state.favoriteCount + 1,
+    });
+  };
+
+  render() {
+    return (
+      <div>
         <h2>{this.props.title}</h2>
         <p>Favorite Count: {this.state.favoriteCount}</p>
-        <img src={this.props.imgSRC} onClick={this.handlefavorite} />
-       </div> 
-      );
-    }
+        <img
+          src={this.props.imgSrc} 
+          alt={this.props.title}
+          onClick={this.handleFavorite}
+        />
+      </div>
+    );
   }
+}
 
 export default Beast;
