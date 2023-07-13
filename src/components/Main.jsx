ad73f582a457bf8b0de1036ba1904af765a576db
Brian Kimbrow <bkimbrow25@gmail.com>
2023-07-12 22:20:28 -0500

    update

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,26 +4,25 @@ import data from "./data.json";
 
 class Main extends React.Component {
 
-    render () {
-        return (
-          let animalComponent = [];
-          data.forEach((animal) => {
-            animalComponent.push (
-                <Beast
-                title={animal.title}
-                imgSrc={animal.image_url}
-                description={animal.description}
-                />
-            );
-         });
-          return (
-            <div>
-                <h2>Animal:</h2>
-                {animalComponent}
-            </div>
+    render() {
+        let animalComponents = [];
+        data.forEach((animal) => {
+          animalComponents.push(
+            <Beast
+              title={animal.title}
+              imgSrc={animal.image_url}
+              description={animal.description}
+            />
           );
-    }
-}
+        });
+        return (
+          <div>
+            <h2>Animal:</h2>
+            {animalComponents}
+          </div>
+        );
+      }
+      
 
 
 
