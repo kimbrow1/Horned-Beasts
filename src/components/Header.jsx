631be8352f7eb81a5f2ce9c2e6b5dbb3dedfb59a
Brian Kimbrow <bkimbrow25@gmail.com>
2023-07-13 22:53:03 -0500

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ class Header extends React.Component {
     render () {
         return (
             <div>
-                <h1>Animals</h1>
+                <h1>ANIMALS 🐕 </h1>
             </div>
         );
     }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,44 +1,64 @@
 import React from "react";
 import Beast from "./Beast";
 import data from "./data.json";
-import Row  from "react-bootstrap/Row";
+import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import  Container  from "react-bootstrap";
-
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 
 
 
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalIsShowing: false,
+      modalHornedBeastName: "", 
+      modalImgUrl: "" 
+    };
+  }
 
-    render() {
-        let animalComponents = [];
-        data.forEach((animal) => {
-          animalComponents.push(
-            <Col>
-            <Beast
-              title={animal.title}
-              imgSrc={animal.image_url}
-              description={animal.description}
-            />
-            </Col>
-          );
-        });
-        return (
-          <Container>
-            <h2>Animal:</h2>
-            <Row>
-            {animalComponents}
-            </Row>
-          </Container>
-        );
-      }
-    } 
+  handleShow = () => {
+    this.setState({ modalIsShowing: true });
+  };
 
+  handleClose = () => {
+    this.setState({ modalIsShowing: false });
+  };
 
-
-
-
-
+  render() {
+    let animalComponents = [];
+    data.forEach((animal) => {
+      animalComponents.push(
+        <Col>
+          <Beast
+            title={animal.title}
+            imgSrc={animal.image_url}
+            description={animal.description}
+          />
+        </Col>
+      );
+    });
+    return (
+      <Container>
+        <Button variant="primary" onClick={this.handleShow}>
+          Launch
+        </Button>
+        <Modal show={this.state.modalIsShowing} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Animal</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <h2>{this.state.modalHornedBeastName}</h2>
+            <img src={this.state.modalImgUrl} alt="Modal Image" />
+          </Modal.Body>
+        </Modal>
+        <Row>{animalComponents}</Row>
+      </Container>
+    );
+  }
+}
 
 export default Main;
