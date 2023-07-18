8dce8182e9a216357bd5b029c7b85b1809e5912a
Brian Kimbrow <bkimbrow25@gmail.com>
2023-07-17 23:04:05 -0500

    selected-horns

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -66,8 +66,6 @@ class Main extends React.Component {
           <option value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
         </select>
         <Row>
           {filteredData.map((beast) => (
